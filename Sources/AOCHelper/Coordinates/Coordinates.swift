1fc60afcec823cbf65c9435c0d5f65b06a45609f
Brent Tunnicliff <brent@tunnicliff.dev>
2024-12-04 22:27:36 +1100

    Added next function for Coordinates that handles diagonal traversal.

--- Sources/AOCHelper/Coordinates/Coordinates.swift
+++ Sources/AOCHelper/Coordinates/Coordinates.swift
@@ -31,12 +31,33 @@ extension Coordinates: Hashable {
 // MARK:
 
 public extension Coordinates {
+    /// Returns the next coordinates in a direction.
     func next(in direction: CompassDirection, distance: Int = 1) -> Coordinates {
         switch direction {
-        case .east: .init(x: x + distance, y: y)
-        case .north: .init(x: x, y: y - distance)
-        case .south: .init(x: x, y: y + distance)
-        case .west: .init(x: x - distance, y: y)
+        case .east: Coordinates(x: x + distance, y: y)
+        case .north: Coordinates(x: x, y: y - distance)
+        case .south: Coordinates(x: x, y: y + distance)
+        case .west: Coordinates(x: x - distance, y: y)
+        }
+    }
+
+    /// Returns the next coordinates in a diagonal direction.
+    ///
+    /// Throws a fatal error if input is invalid. Invalid value is two directions that are the same, or are opposite.
+    func next(in direction: (CompassDirection, CompassDirection), distance: Int = 1) -> Coordinates {
+        switch (direction.0, direction.1) {
+        case (.north, .east), (.east, .north):
+            Coordinates(x: x + distance, y: y - distance)
+        case (.north, .west), (.west, .north):
+            Coordinates(x: x - distance, y: y - distance)
+        case (.south, .east), (.east, .south):
+            Coordinates(x: x + distance, y: y + distance)
+        case (.south, .west), (.west, .south):
+            Coordinates(x: x - distance, y: y + distance)
+        case (.east, .east), (.north, .north), (.south, .south), (.west, .west):
+            fatalError("Directions must be different values: \(direction)")
+        case (.east, .west), (.north, .south), (.south, .north), (.west, .east):
+            fatalError("Directions not be opposite: \(direction)")
         }
     }
 }
--- Tests/AOCHelperTests/Coordinates/CoordinatesTests.swift
+++ Tests/AOCHelperTests/Coordinates/CoordinatesTests.swift
@@ -18,19 +18,46 @@ struct CoordinatesTests {
 
     @Test(arguments: [
         (CompassDirection.east, 1, Coordinates(x: 2, y: 2)),
-        (CompassDirection.east, 20, Coordinates(x: 21, y: 2)),
-        (CompassDirection.east, -20, Coordinates(x: -19, y: 2)),
-        (CompassDirection.north, 1, Coordinates(x: 1, y: 1)),
-        (CompassDirection.north, 20, Coordinates(x: 1, y: -18)),
-        (CompassDirection.north, -20, Coordinates(x: 1, y: 22)),
-        (CompassDirection.south, 1, Coordinates(x: 1, y: 3)),
-        (CompassDirection.south, 20, Coordinates(x: 1, y: 22)),
-        (CompassDirection.south, -20, Coordinates(x: 1, y: -18)),
-        (CompassDirection.west, 1, Coordinates(x: 0, y: 2)),
-        (CompassDirection.west, 20, Coordinates(x: -19, y: 2)),
-        (CompassDirection.west, -20, Coordinates(x: 21, y: 2)),
+        (.east, 20, Coordinates(x: 21, y: 2)),
+        (.east, -20, Coordinates(x: -19, y: 2)),
+        (.north, 1, Coordinates(x: 1, y: 1)),
+        (.north, 20, Coordinates(x: 1, y: -18)),
+        (.north, -20, Coordinates(x: 1, y: 22)),
+        (.south, 1, Coordinates(x: 1, y: 3)),
+        (.south, 20, Coordinates(x: 1, y: 22)),
+        (.south, -20, Coordinates(x: 1, y: -18)),
+        (.west, 1, Coordinates(x: 0, y: 2)),
+        (.west, 20, Coordinates(x: -19, y: 2)),
+        (.west, -20, Coordinates(x: 21, y: 2)),
     ])
-    func testTurnRight(direction: CompassDirection, distance: Int, expectedResult: Coordinates) {
+    func testNext(direction: CompassDirection, distance: Int, expectedResult: Coordinates) {
+        let startingPosition = Coordinates(x: 1, y: 2)
+        let result = startingPosition.next(in: direction, distance: distance)
+        #expect(result == expectedResult)
+    }
+
+    @Test(arguments: [
+        ((CompassDirection.north, CompassDirection.east), 1, Coordinates(x: 2, y: 1)),
+        ((.north, .east), 5, Coordinates(x: 6, y: -3)),
+        ((.east, .north), 1, Coordinates(x: 2, y: 1)),
+        ((.east, .north), 5, Coordinates(x: 6, y: -3)),
+
+        ((.south, .east), 1, Coordinates(x: 2, y: 3)),
+        ((.south, .east), 5, Coordinates(x: 6, y: 7)),
+        ((.east, .south), 1, Coordinates(x: 2, y: 3)),
+        ((.east, .south), 5, Coordinates(x: 6, y: 7)),
+
+        ((.north, .west), 1, Coordinates(x: 0, y: 1)),
+        ((.north, .west), 5, Coordinates(x: -4, y: -3)),
+        ((.west, .north), 1, Coordinates(x: 0, y: 1)),
+        ((.west, .north), 5, Coordinates(x: -4, y: -3)),
+
+        ((.south, .west), 1, Coordinates(x: 0, y: 3)),
+        ((.south, .west), 5, Coordinates(x: -4, y: 7)),
+        ((.west, .south), 1, Coordinates(x: 0, y: 3)),
+        ((.west, .south), 5, Coordinates(x: -4, y: 7)),
+    ])
+    func testNextDiagonal(direction: (CompassDirection, CompassDirection), distance: Int, expectedResult: Coordinates) {
         let startingPosition = Coordinates(x: 1, y: 2)
         let result = startingPosition.next(in: direction, distance: distance)
         #expect(result == expectedResult)
